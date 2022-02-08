3a8e600fb88d410109cedee6c83ae2417906025f
Osama Ahmed <osamaahmed@Osamas-MacBook-Pro.local>
2022-02-08 16:14:43 +0500

    for test purposes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello, World!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <div>Hello World</div>
       </header>
     </div>
   );
